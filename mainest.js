d1a3d425d0e2028dd3c075ce7dd09a21b7105864
eshan18 <50793751+eshan18@users.noreply.github.com>
2023-04-23 18:09:19 +0800

    Add files via upload

---
+++ mainest.js
@@ -0,0 +1,10 @@
+function changecolor(){
+document.getElementById("heading").style.backgroundColor="red";
+document.getElementById("heading").style.color="yellow";
+document.getElementById("heading").style.fontSize="47px";    
+}
+function backtonormal(){
+    document.getElementById("heading").style.backgroundColor="linear-gradient(#000957 0%,#00affa 100%)";
+    document.getElementById("heading").style.color="white";
+    document.getElementById("heading").style.fontSize="47px";    
+    }
